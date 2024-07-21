83101cad8628a1e7e461505246b286e1ed8bf2cd
cybermarmot <cybermarmot1024@gmail.com>
2024-07-21 17:34:16 +0800

    add random fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use gstd::{};
+use gstd::*;
 use pebbles_game_io::*;
 
 static mut PEBBLES_GAME: Option<GameState> = None;
@@ -8,6 +8,7 @@ static mut PEBBLES_GAME: Option<GameState> = None;
 #[no_mangle]
 extern "C" fn init(){
     // YOUR CODE HERE
+    let random = get_random_u32();
 }
 
 #[no_mangle]
@@ -18,4 +19,10 @@ extern "C" fn handle(){
 #[no_mangle]
 extern "C" fn state(){
     // YOUR CODE HERE
+}
+
+fn get_random_u32() -> u32 {
+    let salt = msg::id();
+    let (hash, _num) = exec::random(salt.into()).expect("get_random_u32(): random call failed");
+    u32::from_le_bytes([hash[0], hash[1], hash[2], hash[3]])
 }
